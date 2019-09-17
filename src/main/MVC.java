61727c034e76ccc98ac3f32ca0bdb9849e37c7f1
Andres Bayona <al.bayona@uniandes.edu.co>
2019-09-16 23:28:31 -0500

    Changes

--- src/main/MVC.java
+++ src/main/MVC.java
@@ -1,15 +1,21 @@
 package main;
 import controller.Controller;
-import model.data_structures.IListaIterador;
 import model.data_structures.ListaEncadenada;
 import model.logic.MVCModelo;
+import model.logic.Viaje;
+
 
 public class MVC {
-	
-	public static void main(String[] args) 
+
+
+	public static void main(String[] args)
 	{
-		
+
 		Controller controler = new Controller();
 		controler.run();
+
+
+
+
 	}
 }
