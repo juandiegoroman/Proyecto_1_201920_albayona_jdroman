d0d2123937a36c9e92be974b687631a814e30d7e
Andres Bayona <al.bayona@uniandes.edu.co>
2019-09-17 18:59:53 -0500

    Changes

--- src/main/MVC.java
+++ src/main/MVC.java
@@ -1,6 +1,8 @@
 package main;
 import controller.Controller;
-
+import model.data_structures.ListaEncadenada;
+import model.logic.MVCModelo;
+import model.logic.Viaje;
 
 
 public class MVC {
@@ -11,6 +13,13 @@ public class MVC {
 
 		Controller controler = new Controller();
 		controler.run();
+
+//		MVCModelo mod = new MVCModelo();
+//
+//		mod.cargarDatosPorTrimiestre(1);
+//		mod.compararConsultasEnAmbosSentidos(1,0,100, 1, MVCModelo.DIA);
+
+
 
 	}
 }
