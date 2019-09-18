6d835a629cfc014c88281cd34d1a8bb7b1066347
Andres Bayona <al.bayona@uniandes.edu.co>
2019-09-17 19:36:47 -0500

    Changes

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -115,16 +115,16 @@ public class Controller {
                     }
 
                     System.out.println("Para el mes " + mes + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de " + modelo.tiempoPromedioDeViajesEnRangoPorIndicadorTemporal(mes, zonaOrigen, zonaDestino, MVCModelo.MES) + " segundos");
-					System.out.println("Para el mes " + mes + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de desviacion estandar de " + modelo.promedioDesviacionEstandarDeViajesEnRangoPorIndicadorTemporal(mes, zonaOrigen, zonaDestino, MVCModelo.MES) );
-					System.out.println();
-					System.out.println("Para el dia " + dia + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de " + modelo.tiempoPromedioDeViajesEnRangoPorIndicadorTemporal(dia, zonaOrigen, zonaDestino, MVCModelo.DIA) + " segundos");
-					System.out.println("Para el dia " + dia + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de desviacion estandar de " + modelo.promedioDesviacionEstandarDeViajesEnRangoPorIndicadorTemporal(dia, zonaOrigen, zonaDestino, MVCModelo.DIA) );
-					System.out.println();
-					System.out.println("Para la hora " + hora + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de " + modelo.tiempoPromedioDeViajesEnRangoPorIndicadorTemporal(hora, zonaOrigen, zonaDestino, MVCModelo.HORA) + " segundos");
-					System.out.println("Para la hora " + hora + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de desviacion estandar de " + modelo.promedioDesviacionEstandarDeViajesEnRangoPorIndicadorTemporal(hora, zonaOrigen, zonaDestino, MVCModelo.HORA) );
-					System.out.println();
+                    System.out.println("Para el mes " + mes + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de desviacion estandar de " + modelo.promedioDesviacionEstandarDeViajesEnRangoPorIndicadorTemporal(mes, zonaOrigen, zonaDestino, MVCModelo.MES));
+                    System.out.println();
+                    System.out.println("Para el dia " + dia + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de " + modelo.tiempoPromedioDeViajesEnRangoPorIndicadorTemporal(dia, zonaOrigen, zonaDestino, MVCModelo.DIA) + " segundos");
+                    System.out.println("Para el dia " + dia + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de desviacion estandar de " + modelo.promedioDesviacionEstandarDeViajesEnRangoPorIndicadorTemporal(dia, zonaOrigen, zonaDestino, MVCModelo.DIA));
+                    System.out.println();
+                    System.out.println("Para la hora " + hora + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de " + modelo.tiempoPromedioDeViajesEnRangoPorIndicadorTemporal(hora, zonaOrigen, zonaDestino, MVCModelo.HORA) + " segundos");
+                    System.out.println("Para la hora " + hora + ", los viajes entre la zona " + zonaOrigen + " y la zona " + zonaDestino + " tienen un promedio de desviacion estandar de " + modelo.promedioDesviacionEstandarDeViajesEnRangoPorIndicadorTemporal(hora, zonaOrigen, zonaDestino, MVCModelo.HORA));
+                    System.out.println();
 
-					break;
+                    break;
 
                 case 3:
 
@@ -162,15 +162,15 @@ public class Controller {
                     }
 
                     System.out.print(" \n Para el mes " + mes + ", los " + nViajes + " viajes (si los hay) con mayor tiempo promedio son: \n ");
-                    printList(modelo.darNMayoresPorIndicadorTemporal(nViajes,mes, MVCModelo.MES).iterador());
+                    printList(modelo.darNMayoresPorIndicadorTemporal(nViajes, mes, MVCModelo.MES).iterador());
                     System.out.print(" \n Para el dia " + dia + ", los " + nViajes + " viajes (si los hay) con mayor tiempo promedio son: \n ");
-                    printList(modelo.darNMayoresPorIndicadorTemporal(nViajes,dia, MVCModelo.DIA).iterador());
+                    printList(modelo.darNMayoresPorIndicadorTemporal(nViajes, dia, MVCModelo.DIA).iterador());
                     System.out.print(" \n Para la hora " + hora + ", los " + nViajes + " viajes (si los hay) con mayor tiempo promedio son: \n ");
-                    printList(modelo.darNMayoresPorIndicadorTemporal(nViajes, hora,MVCModelo.HORA).iterador());
+                    printList(modelo.darNMayoresPorIndicadorTemporal(nViajes, hora, MVCModelo.HORA).iterador());
 
                     break;
 
-                case  4:
+                case 4:
                     try {
                         System.out.println("\nIngresar una zona dada de referencia: ");
                         zonaOrigen = lector.nextInt();
@@ -211,18 +211,63 @@ public class Controller {
                         System.out.print("Debe ingresar un n�mero.\n");
                     }
 
-                    System.out.println("Para el mes " + mes + ", se calculo la siguiente comparacion: " );
+                    System.out.println("Para el mes " + mes + ", se calculo la siguiente comparacion: ");
                     System.out.println();
-                    modelo.compararConsultasEnAmbosSentidos(zonaOrigen, zonaMenor,zonaMayor, mes,MVCModelo.MES);
+                    modelo.compararConsultasEnAmbosSentidos(zonaOrigen, zonaMenor, zonaMayor, mes, MVCModelo.MES);
                     System.out.println();
-                    System.out.println("Para el dia " + dia + ", se calculo la siguiente comparacion: " );
+                    System.out.println("Para el dia " + dia + ", se calculo la siguiente comparacion: ");
                     System.out.println();
-                    modelo.compararConsultasEnAmbosSentidos(zonaOrigen, zonaMenor,zonaMayor, dia, MVCModelo.DIA);
+                    modelo.compararConsultasEnAmbosSentidos(zonaOrigen, zonaMenor, zonaMayor, dia, MVCModelo.DIA);
                     System.out.println();
                     break;
 
 
-				default: {
+                case 5:
+
+
+
+
+
+                    System.out.println("Aproximación en minutos de viajes entre zona origen y zona destino.");
+                    System.out.println("Trimestre " + trimestre + " del 2018 detallado por cada hora del día");
+
+                    System.out.println("Zona origen: " + zonaOrigen);
+                    System.out.println("Zona destino: " + zonaDestino);
+                    System.out.printf(" %-10s  %20s", "Hora|", "# de mminutos");
+
+                    try {
+                        System.out.println("\nIngresar la zona de origen: ");
+                        zonaOrigen = lector.nextInt();
+                    } catch (Exception e) {
+
+                        System.out.print("Debe ingresar un n�mero.\n");
+                    }
+
+                    try {
+                        System.out.println("\nIngresar la zona de destino: ");
+                        zonaDestino = lector.nextInt();
+                    } catch (Exception e) {
+
+                        System.out.print("Debe ingresar un n�mero.\n");
+                    }
+
+                    int[] temp = modelo.datosGrafica(zonaOrigen, zonaDestino);
+
+                    for (int i = 0; i < temp.length; i++) {
+
+                        String linea = "";
+
+                        for (int j = 0; j <  temp[i]; j++) {
+                            linea+="*";
+                        }
+
+
+                        System.out.printf(" %-10s  %20s", String.format("%04d", i) + "\r|", linea);
+                        System.out.println();
+                    }
+
+
+                default: {
                     System.out.println("Opcion invalida");
                 }
             }
@@ -237,10 +282,12 @@ public class Controller {
         System.out.println("----------------------------------------------------------------------------------------");
         while (iter.haySiguiente()) {
             actual = iter.siguiente();
-            System.out.format("%10s  %20s  %20s %20s",  actual.darIdOrigen(), actual.darIdDestino(), actual.darTiempoPromedio(), actual.darDesviacionEstandar());
+            System.out.format("%10s  %20s  %20s %20s", actual.darIdOrigen(), actual.darIdDestino(), actual.darTiempoPromedio(), actual.darDesviacionEstandar());
             System.out.println();
         }
     }
+
+
 
 
 }
