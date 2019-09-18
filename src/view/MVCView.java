9acb963484873b2f650bf15a14472515d56b32c1
unknown <jd.roman@uniandes.edu.co>
2019-09-17 19:48:13 -0500

    MVC view

--- src/view/MVCView.java
+++ src/view/MVCView.java
@@ -15,6 +15,10 @@ public class MVCView
 		public void printMenu()
 		{
 			System.out.println("1. Cargar los viajes para el trimiestre seleccionado.");
+			System.out.println("2. Tiempo promedio y desviacion estandar en rango indicado.");
+			System.out.println("3. Dar N viajes con mayor tiempo promedio.");
+			System.out.println("4. Comparar consultas de zona X con zona dada.");
+			System.out.println("5. Tabla ASCII.");
 		}
 
 		public void printMessage(String mensaje) {
